27d8b2f03d29ba6dde0b0daed895bf0622dbc5a9
何全 <hequan2011@sina.com>
2018-06-03 18:43:38 +0800

    2
    2

--- chain/celery.py
+++ chain/celery.py
@@ -11,7 +11,7 @@ app = Celery('chain')
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()

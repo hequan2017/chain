e8e983f83ac4f5120be88983bbb73f447bd375c4
何全 <hequan2011@sina.com>
2018-06-04 23:05:36 +0800

    0.2.6.1 安装bug修复。
    0.2.6.1 安装bug修复。

--- crontab/admin.py
+++ crontab/admin.py
@@ -1,13 +1,2 @@
 
 from django.contrib import admin
-from django_celery_results.models import TaskResult
-
-
-
-class TaskMetaAdmin(admin.ModelAdmin):
-    search_fields = ('task_id', 'status', 'result', 'date_done',)
-    list_display = ('task_id', 'status', 'result', 'date_done',)
-    list_display_links = ('task_id', 'status', 'result', 'date_done',)
-    list_filter = ('task_id', 'status', 'date_done',)
-
-admin.site.register(TaskResult, TaskMetaAdmin)

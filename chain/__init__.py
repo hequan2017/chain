6aaa5601a45a9f6972069b4a4a9f0b1962b4c34b
何全 <hequan2011@sina.com>
2019-03-15 14:28:44 +0800

    Update __init__.py

--- chain/__init__.py
+++ chain/__init__.py
@@ -4,4 +4,7 @@ from __future__ import absolute_import, unicode_literals
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 __all__ = ['celery_app']

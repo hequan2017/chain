e69f2de2e223b5005d52dc2d5c61f4959adefbf8
何全 <hequan@aipark.com>
2019-06-03 15:04:24 +0800

    update

--- chain/__init__.py
+++ chain/__init__.py
@@ -4,7 +4,9 @@ from __future__ import absolute_import, unicode_literals
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app
 
-import pymysql
-pymysql.install_as_MySQLdb()
+# import pymysql
+# pymysql.install_as_MySQLdb()
+
+import MySQLdb
 
 __all__ = ['celery_app']
--- chain/settings.py
+++ chain/settings.py
@@ -97,23 +97,25 @@ WSGI_APPLICATION = 'chain.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+import socket
+if socket.gethostname().endswith('demo'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'NAME': 'seal',
+            'USER': 'root',
+            'PASSWORD': '123456',
+        }
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 }
-
-# DATABASES = {
-#      'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'chain',
-#         'USER': 'root',
-#         'PASSWORD': '111111',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#      }
-# }
 
 
 # Password validation
